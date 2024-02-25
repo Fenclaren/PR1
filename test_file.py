ab3d715576f35cf5419a11167300647ed9ae622c
Fenclaren <82043003+Fenclaren@users.noreply.github.com>
2024-02-25 20:11:36 +0300

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,10 +1,10 @@
 from app_file import mult
 
 def test_mult_0():
-    assert mult(5,8) == 40
+    assert mult(5,8) == 45
   
 def test_mult_1():
-    assert mult(2,4) == 8
+    assert mult(2,4) == 3
   
 def test_mult_2():
     assert mult(5,5) == 25
